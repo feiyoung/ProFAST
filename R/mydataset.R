26430298ebcc1adf81a45aa2066a67af1932bd33
feiyoung <1029725413@qq.com>
2024-03-17 18:40:10 +0800

    'update'

--- R/mydataset.R
+++
@@ -1,9 +0,0 @@
-#' A Seurat object including spatial transcriptomics dataset from CosMx platform  
-#'  
-#' This data is a subset of  SCLC CosMx spatial transcriptomics  dataset.
-#'  
-#' @format A Seurat object, including count matrix, sptial coordinates, and manual annotation.
-#' @source The data is from the CosMx platform.
-#' @references None 
-#' @usage data(CosMx_subset)  
-
